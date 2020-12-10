e0cfae9d0f71c871e1b34f593053b31f870108c5
Everaldo Júnior <39379021+everaldojunior20@users.noreply.github.com>
2020-12-10 14:27:13 -0300

    Update UsersBean.java

--- src/main/java/br/com/testeonline/bean/UsersBean.java
+++ src/main/java/br/com/testeonline/bean/UsersBean.java
@@ -14,6 +14,7 @@ import br.com.testeonline.to.UsersTO;
 @SessionScoped
 public class UsersBean {
 	
+	
 	private Users usuario = new Users();
 	private UsersTO usersTO = new UsersTO();
 	
@@ -24,8 +25,8 @@ public class UsersBean {
 		usuario = usersTO.getUsuario(usuario);
 		if (usuario == null) {
 			usuario = new Users();
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usu·rio n„o encontrado!", "Erro no Login!"));
-			log.info("LoginMB - login() - Usu·rio n„o encontrado!");
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usu√°rio n√£o encontrado!", "Erro no Login!"));
+			log.info("LoginMB - login() - Usu√°rio n√£o encontrado!");
 			return null;
 		} else {
 			return "index?faces-redirect=true";
