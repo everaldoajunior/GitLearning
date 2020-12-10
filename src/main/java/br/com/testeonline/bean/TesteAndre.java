2525553fbf638ab4665c8cd86255699ccf5444a4
Everaldo Júnior <everaldo.junior@tascominformatica.com.br>
2020-12-10 15:19:41 -0300

    MENSAGEM PADRÃO

--- src/main/java/br/com/testeonline/bean/TesteAndre.java
+++ src/main/java/br/com/testeonline/bean/TesteAndre.java
@@ -5,4 +5,19 @@ public class TesteAndre {
 	String nome;
 	int idade;
 	
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
