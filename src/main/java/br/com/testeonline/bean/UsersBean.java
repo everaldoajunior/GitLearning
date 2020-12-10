b078875b92b727376e33d9b003b6eeb955c6805a
Everaldo Júnior <everaldo.junior@tascominformatica.com.br>
2020-12-10 14:36:46 -0300

    Teste conflito

--- src/main/java/br/com/testeonline/bean/UsersBean.java
+++ src/main/java/br/com/testeonline/bean/UsersBean.java
@@ -14,6 +14,7 @@ import br.com.testeonline.to.UsersTO;
 @SessionScoped
 public class UsersBean {
 	
+	private boolean status = false;
 	private Users usuario = new Users();
 	private UsersTO usersTO = new UsersTO();
 	
